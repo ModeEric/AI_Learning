e466078cd5d3bc429a3d04eee1fc52dce6ede3bf
ModeEric <ericjm4@illinois.edu>
2024-11-10 10:26:10 -0600

    Too hard for now and quite busy atm, will push back

--- rl_from_scratch/ppo_from_scratch.py
+++ rl_from_scratch/ppo_from_scratch.py
@@ -1,1 +1,5 @@
 #Todo, following along from this video: https://www.youtube.com/watch?v=hlv79rcHws0
+
+#A bit above my understanding at the moment, will push back.
+class PPOMemory:
+    def __init__()
